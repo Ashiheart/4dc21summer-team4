7baed1f1bb9d97ef4ff9bc4b34e42563f66cf38c
38cyclamen <s20a5019bv@s.chibakoudai.jp>
2021-08-05 00:16:46 +0900

    create move
    初期位置と初速度を変えられるようにしました

---
+++ chikuwa/Assets/Scenes/Personal/Cyclamen/TestFolder/Move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] float InitVelocity;
+    [SerializeField] float InitPosition;
+    float timer;
+    float pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(InitPosition, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(InitVelocity, 0, 0);
+    }
+
+    void Pinch(float timer, float pos)
+    {
+        float i;
+        for (i = 0; i <= timer; i += Time.deltaTime)
+        {
+            transform.position += new Vector3(InitVelocity, 0, 0);
+        }
+    }
+}
