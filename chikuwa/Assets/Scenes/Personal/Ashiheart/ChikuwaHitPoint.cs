1477bb20d0dc9df2d19bf539fa9f367c60195e44
Ashiheart <extrmediamond@yahoo.co.jp>
2021-08-07 07:52:39 +0900

    public int hp
    bigじゃない方のちくわのhpもpublicにしました。

---
+++ chikuwa/Assets/Scenes/Personal/Ashiheart/ChikuwaHitPoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class ChikuwaHitPoint : MonoBehaviour
+{
+
+
+    private IntReactiveProperty currentHitPoint;
+    public IReadOnlyReactiveProperty<int> CurrentHitPoint => currentHitPoint;
+
+    [SerializeField] private BigtikuwaMove bigChikuwaStatus;
+
+    [SerializeField] private CharacterMove smallChikuwaStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.ObserveEveryValueChanged()
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
