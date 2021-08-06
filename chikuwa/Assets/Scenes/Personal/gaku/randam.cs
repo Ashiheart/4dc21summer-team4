474c7a511e462148374eb928a1afa4bb77434253
gaku-code <77351288+gaku-code@users.noreply.github.com>
2021-08-06 20:22:16 +0900

    Fix ramdom
    右から指定した複数のオブジェクトを生成して左へ移動させるようにしました。

---
+++ chikuwa/Assets/Scenes/Personal/gaku/randam.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randam : MonoBehaviour
+{
+    public GameObject[] Meteo;
+    private int number;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Make", 1f); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Make()
+    {
+        Vector3 randomPos = Vector3.zero;
+        randomPos.y = Random.Range(-20, 20);
+
+        number = Random.Range(0, Meteo.Length);
+        Instantiate(Meteo[number], transform.position + randomPos, transform.rotation);
+        Invoke("Make", 1f);
+    }
+}
