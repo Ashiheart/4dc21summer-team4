ed8eb1947d05a1343d83d13032a4ee4a917db116
pekepekee <takkummm78@gmail.com>
2021-08-07 01:07:56 +0900

    Add character change & hp & dog collision detection & obstacle collision detection
    タラを3個取ったらちくわが大きくなるのを実装した。
    ちくわにHPを追加した。
    犬に当たったときにゲームオーバーシーンに行くようにした。
    障害物にぶつかったときにHPが減るようにした。また、HPが０になったときにゲームオーバーシーンに行くようにした。

---
+++ chikuwa/Assets/Scenes/Personal/pekepeke/CharacterChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterChange : MonoBehaviour
+{
+    //　現在どのキャラクターを操作しているか
+    private int nowChara;
+    //　操作可能なゲームキャラクター
+    [SerializeField]
+    private List<GameObject> charaList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        charaList[0].SetActive(true);
+        charaList[1].SetActive(false);
+        charaList[0].GetComponent<CharacterMove>();
+    }
+
+    public void ChangeCharacter(int tempNowChara)
+    {
+        //　現在操作しているキャラクターを動かなくする
+        charaList[tempNowChara].SetActive(false);
+        //　次のキャラクターの番号を設定
+        var nextChara = tempNowChara + 1;
+
+        //　次のキャラクターを動かせるようにする
+        charaList[nextChara].SetActive(true);
+        charaList[nextChara].GetComponent<CharacterMove>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
