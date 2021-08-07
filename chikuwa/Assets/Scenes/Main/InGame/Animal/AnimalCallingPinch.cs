5015f00d59951a84a93abdd3832ad2cafc7961eb
Ashiheart <extrmediamond@yahoo.co.jp>
2021-08-07 09:08:31 +0900

    スクリプトファイル移動、HPの減少に応じてPinchを呼び出し

---
+++ chikuwa/Assets/Scenes/Main/InGame/Animal/AnimalCallingPinch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class AnimalCallingPinch : MonoBehaviour
+{
+    [SerializeField] private ChikuwaHitPoint chikuwaHp;
+
+    [SerializeField] private AnimalMove animalMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (chikuwaHp == null) Debug.LogError("ChikuwaHp is null");
+
+        chikuwaHp
+            .CurrentHitPoint
+            .Subscribe(_ => animalMove.Pinch(animalMove.timer, animalMove.pos))
+            ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
