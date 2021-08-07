ea87c7939d9529767a6cefb335bf98a2dec70cb5
pekepekee <takkummm78@gmail.com>
2021-08-07 11:59:56 +0900

    Add GameOverScene sound
    ゲームオーバーシーンのボタンに選択音と決定音を追加しました

--- chikuwa/Assets/Scenes/Main/GameOver/SelectButton2.cs
+++ chikuwa/Assets/Scenes/Main/GameOver/SelectButton2.cs
@@ -3,13 +3,24 @@ using UnityEngine;
 
 public class SelectButton2 : MonoBehaviour
 {
+    [SerializeField] private AudioClip choose;
+
+    [SerializeField] private AudioClip down;
+
+    public void ButtonChose()
+    {
+        GetComponent<AudioSource>().PlayOneShot(choose);
+    }
+
     public void ClickRetryGame()
     {
+        GetComponent<AudioSource>().PlayOneShot(down);
         SceneManager.LoadScene("InGame");
     }
 
     public void ClickTitleGame()
     {
+        GetComponent<AudioSource>().PlayOneShot(down);
         SceneManager.LoadScene("Title");
     }
 }
